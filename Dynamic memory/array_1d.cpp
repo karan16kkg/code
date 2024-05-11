0a86cbf0243f0c4c41015b473f7b5718556cc428
karan16kkg <122518971+karan16kkg@users.noreply.github.com>
2024-05-11 18:28:26 +0530

    Add files via upload

---
+++ Dynamic memory/array_1d.cpp
@@ -0,0 +1,16 @@
+//making an array by dynamic memory allocation
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int *arr=new int[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    delete[]arr;
+    return 0;
+}
